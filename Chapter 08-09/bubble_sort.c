1a4b73dbc90ab2fdb6803d8f622726bb1a0835b6
euncheol_97 <1997c0703@gmail.com>
2022-01-05 23:19:58 +0900

    added 4 files in chapter 08-09 folder

---
+++ Chapter 08-09/bubble_sort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int aList[5] = { 30 ,40, 10, 50, 20 };
+	int i = 0, j = 0, nTmp = 0;
+
+	for (i = 4; i > 0; --i)
+	{
+		for (j = 0; j < i; ++j)
+		{
+			if (aList[j] > aList[j + 1])
+			{
+				nTmp = aList[j];
+				aList[j] = aList[j + 1];
+				aList[j + 1] = nTmp;
+			}
+		} putchar('\n');
+	}
+
+
+
+	for (i = 0; i < 5; ++i)
+	{
+		printf("%d\t", aList[i]);
+
+	}
+	putchar('\t');
+	return 0;
+
+}
